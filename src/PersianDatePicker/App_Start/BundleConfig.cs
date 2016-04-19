280354fd3d05afc0ead1b77a32ec0f4bd477e5ef
unknown <930919@khosravifar-B.dbitabriz.local>
2016-04-19 16:46:51 +0430

    first init

---
+++ src/PersianDatePicker/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PersianDatePicker
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.min.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/PersianDateTimePicker").Include("~/Scripts/MdBootstrapPersianDateTimePicker/jquery.Bootstrap-PersianDateTimePicker.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jalaali").Include("~/Scripts/MdBootstrapPersianDateTimePicker/jalaali.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.min.css"));
+            
+            bundles.Add(new StyleBundle("~/Content/PersianDateTimePicker").Include("~/Content/MdBootstrapPersianDateTimePicker/jquery.Bootstrap-PersianDateTimePicker.min.css"));
+        }
+    }
+}
