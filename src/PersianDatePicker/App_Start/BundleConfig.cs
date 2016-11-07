1811e854e3545a6b125da70b0595c4559849a7d0
Behzad Khosravifar <Behzad.khosravifar@gmail.com>
2016-11-07 14:42:18 +0330

    Reduce some unneccess files and add getter func for datetime in js

--- src/PersianDatePicker/App_Start/BundleConfig.cs
+++ src/PersianDatePicker/App_Start/BundleConfig.cs
@@ -8,19 +8,18 @@ namespace PersianDatePicker
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.min.js"));
-
-
-            bundles.Add(new ScriptBundle("~/bundles/PersianDateTimePicker").Include("~/Scripts/MdBootstrapPersianDateTimePicker/jquery.Bootstrap-PersianDateTimePicker.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jalaali").Include("~/Scripts/MdBootstrapPersianDateTimePicker/jalaali.js"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/MdBootstrapPersianDateTimePicker/jquery.Bootstrap-PersianDateTimePicker.js",
+                "~/Scripts/MdBootstrapPersianDateTimePicker/jalaali.js"
+                ));
 
 
-            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.min.css"));
-            
-            bundles.Add(new StyleBundle("~/Content/PersianDateTimePicker").Include("~/Content/MdBootstrapPersianDateTimePicker/jquery.Bootstrap-PersianDateTimePicker.min.css"));
+            bundles.Add(new StyleBundle("~/Content/styles").Include(
+                "~/Content/bootstrap.min.css",
+                "~/Content/MdBootstrapPersianDateTimePicker/jquery.Bootstrap-PersianDateTimePicker.min.css"));
+
         }
     }
 }
--- src/PersianDatePicker/Scripts/MdBootstrapPersianDateTimePicker/jalaali.js
+++ src/PersianDatePicker/Scripts/MdBootstrapPersianDateTimePicker/jalaali.js
@@ -2,31 +2,33 @@
   Expose functions.
   https://github.com/jalaali/jalaali-js
 */
-//module.exports =
-//  { toJalaali: toJalaali
-//  , toGregorian: toGregorian
-//  , isValidJalaaliDate: isValidJalaaliDate
-//  , isLeapJalaaliYear: isLeapJalaaliYear
-//  , jalaaliMonthLength: jalaaliMonthLength
-//  , jalCal: jalCal
-//  , j2d: j2d
-//  , d2j: d2j
-//  , g2d: g2d
-//  , d2g: d2g
-//  }
+
+module.exports =
+  {
+      toJalaali: toJalaali
+  , toGregorian: toGregorian
+  , isValidJalaaliDate: isValidJalaaliDate
+  , isLeapJalaaliYear: isLeapJalaaliYear
+  , jalaaliMonthLength: jalaaliMonthLength
+  , jalCal: jalCal
+  , j2d: j2d
+  , d2j: d2j
+  , g2d: g2d
+  , d2g: d2g
+  }
 
 /*
   Converts a Gregorian date to Jalaali.
 */
 function toJalaali(gy, gm, gd) {
-    return d2j(g2d(gy, gm, gd));
+    return d2j(g2d(gy, gm, gd))
 }
 
 /*
   Converts a Jalaali date to Gregorian.
 */
 function toGregorian(jy, jm, jd) {
-    return d2g(j2d(jy, jm, jd));
+    return d2g(j2d(jy, jm, jd))
 }
 
 /*
@@ -34,25 +36,25 @@ function toGregorian(jy, jm, jd) {
 */
 function isValidJalaaliDate(jy, jm, jd) {
     return jy >= -61 && jy <= 3177 &&
-        jm >= 1 && jm <= 12 &&
-        jd >= 1 && jd <= jalaaliMonthLength(jy, jm);
+            jm >= 1 && jm <= 12 &&
+            jd >= 1 && jd <= jalaaliMonthLength(jy, jm)
 }
 
 /*
   Is this a leap year or not?
 */
 function isLeapJalaaliYear(jy) {
-    return jalCal(jy).leap === 0;
+    return jalCal(jy).leap === 0
 }
 
 /*
   Number of days in a given month in a Jalaali year.
 */
 function jalaaliMonthLength(jy, jm) {
-    if (jm <= 6) return 31;
-    if (jm <= 11) return 30;
-    if (isLeapJalaaliYear(jy)) return 30;
-    return 29;
+    if (jm <= 6) return 31
+    if (jm <= 11) return 30
+    if (isLeapJalaaliYear(jy)) return 30
+    return 29
 }
 
 /*
@@ -70,55 +72,61 @@ function jalaaliMonthLength(jy, jm) {
   @see: http://www.fourmilab.ch/documents/calendar/
 */
 function jalCal(jy) {
-  // Jalaali years starting the 33-year rule.
-    var breaks = [-61, 9, 38, 199, 426, 686, 756, 818, 1111, 1181, 1210, 1635, 2060, 2097, 2192, 2262, 2324, 2394, 2456, 3178],
-        bl = breaks.length,
-        gy = jy + 621,
-        leapJ = -14,
-        jp = breaks[0],
-        jm,
-        jump = 1,
-        leap,
-        n,
-        i;
+    // Jalaali years starting the 33-year rule.
+    var breaks = [-61, 9, 38, 199, 426, 686, 756, 818, 1111, 1181, 1210
+                  , 1635, 2060, 2097, 2192, 2262, 2324, 2394, 2456, 3178
+    ]
+      , bl = breaks.length
+      , gy = jy + 621
+      , leapJ = -14
+      , jp = breaks[0]
+      , jm
+      , jump
+      , leap
+      , leapG
+      , march
+      , n
+      , i
 
     if (jy < jp || jy >= breaks[bl - 1])
-        throw new Error('Invalid Jalaali year ' + jy);
+        throw new Error('Invalid Jalaali year ' + jy)
 
-  // Find the limiting years for the Jalaali year jy.
-  for (i = 1; i < bl; i += 1) {
-      jm = breaks[i];
-      jump = jm - jp;
-      if (jy < jm)
-          break;
-      leapJ = leapJ + div(jump, 33) * 8 + div(mod(jump, 33), 4);
-      jp = jm;
-  }
-    n = jy - jp;
+    // Find the limiting years for the Jalaali year jy.
+    for (i = 1; i < bl; i += 1) {
+        jm = breaks[i]
+        jump = jm - jp
+        if (jy < jm)
+            break
+        leapJ = leapJ + div(jump, 33) * 8 + div(mod(jump, 33), 4)
+        jp = jm
+    }
+    n = jy - jp
 
-  // Find the number of leap years from AD 621 to the beginning
-  // of the current Jalaali year in the Persian calendar.
-    leapJ = leapJ + div(n, 33) * 8 + div(mod(n, 33) + 3, 4);
+    // Find the number of leap years from AD 621 to the beginning
+    // of the current Jalaali year in the Persian calendar.
+    leapJ = leapJ + div(n, 33) * 8 + div(mod(n, 33) + 3, 4)
     if (mod(jump, 33) === 4 && jump - n === 4)
-        leapJ += 1;
+        leapJ += 1
 
-  // And the same in the Gregorian calendar (until the year gy).
-  var leapG = div(gy, 4) - div((div(gy, 100) + 1) * 3, 4) - 150;
+    // And the same in the Gregorian calendar (until the year gy).
+    leapG = div(gy, 4) - div((div(gy, 100) + 1) * 3, 4) - 150
 
-  // Determine the Gregorian date of Farvardin the 1st.
-  var march = 20 + leapJ - leapG;
+    // Determine the Gregorian date of Farvardin the 1st.
+    march = 20 + leapJ - leapG
 
-  // Find how many years have passed since the last leap year.
+    // Find how many years have passed since the last leap year.
     if (jump - n < 6)
-        n = n - jump + div(jump + 4, 33) * 33;
-    leap = mod(mod(n + 1, 33) - 1, 4);
-  if (leap === -1) leap = 4;
+        n = n - jump + div(jump + 4, 33) * 33
+    leap = mod(mod(n + 1, 33) - 1, 4)
+    if (leap === -1) {
+        leap = 4
+    }
 
     return {
-        leap: leap,
-        gy: gy,
-        march: march
-    };
+        leap: leap
+            , gy: gy
+            , march: march
+    }
 }
 
 /*
@@ -130,8 +138,8 @@ function jalCal(jy) {
   @return Julian Day number
 */
 function j2d(jy, jm, jd) {
-    var r = jalCal(jy);
-    return g2d(r.gy, 3, r.march) + (jm - 1) * 31 - div(jm, 7) * (jm - 7) + jd - 1;
+    var r = jalCal(jy)
+    return g2d(r.gy, 3, r.march) + (jm - 1) * 31 - div(jm, 7) * (jm - 7) + jd - 1
 }
 
 /*
@@ -144,44 +152,44 @@ function j2d(jy, jm, jd) {
     jd: Jalaali day (1 to 29/31)
 */
 function d2j(jdn) {
-    var gy = d2g(jdn).gy, // Calculate Gregorian year (gy).
-        jy = gy - 621,
-        r = jalCal(jy),
-        jdn1F = g2d(gy, 3, r.march),
-        jd,
-        jm,
-        k;
+    var gy = d2g(jdn).gy // Calculate Gregorian year (gy).
+      , jy = gy - 621
+      , r = jalCal(jy)
+      , jdn1f = g2d(gy, 3, r.march)
+      , jd
+      , jm
+      , k
 
-  // Find number of days that passed since 1 Farvardin.
-    k = jdn - jdn1F;
-  if (k >= 0) {
-    if (k <= 185) {
-      // The first 6 months.
-        jm = 1 + div(k, 31);
-        jd = mod(k, 31) + 1;
-        return {
-            jy: jy,
-            jm: jm,
-            jd: jd
-        };
+    // Find number of days that passed since 1 Farvardin.
+    k = jdn - jdn1f
+    if (k >= 0) {
+        if (k <= 185) {
+            // The first 6 months.
+            jm = 1 + div(k, 31)
+            jd = mod(k, 31) + 1
+            return {
+                jy: jy
+                    , jm: jm
+                    , jd: jd
+            }
+        } else {
+            // The remaining months.
+            k -= 186
+        }
     } else {
-      // The remaining months.
-        k -= 186;
+        // Previous Jalaali year.
+        jy -= 1
+        k += 179
+        if (r.leap === 1)
+            k += 1
     }
-  } else {
-    // Previous Jalaali year.
-      jy -= 1;
-      k += 179;
-      if (r.leap === 1)
-          k += 1;
-  }
-    jm = 7 + div(k, 30);
-    jd = mod(k, 30) + 1;
+    jm = 7 + div(k, 30)
+    jd = mod(k, 30) + 1
     return {
-        jy: jy,
-        jm: jm,
-        jd: jd
-    };
+        jy: jy
+            , jm: jm
+            , jd: jd
+    }
 }
 
 /*
@@ -199,9 +207,9 @@ function d2j(jdn) {
 function g2d(gy, gm, gd) {
     var d = div((gy + div(gm - 8, 6) + 100100) * 1461, 4)
         + div(153 * mod(gm + 9, 12) + 2, 5)
-        + gd - 34840408;
-    d = d - div(div(gy + 100100 + div(gm - 8, 6), 100) * 3, 4) + 752;
-    return d;
+        + gd - 34840408
+    d = d - div(div(gy + 100100 + div(gm - 8, 6), 100) * 3, 4) + 752
+    return d
 }
 
 /*
@@ -216,18 +224,22 @@ function g2d(gy, gm, gd) {
     gd: Calendar day of the month M (1 to 28/29/30/31)
 */
 function d2g(jdn) {
-    var j;
-    j = 4 * jdn + 139361631;
-    j = j + div(div(4 * jdn + 183187720, 146097) * 3, 4) * 4 - 3908;
-  var i = div(mod(j, 1461), 4) * 5 + 308;;
-  var gd = div(mod(i, 153), 5) + 1;
-  var gm = mod(div(i, 153), 12) + 1;
-  var gy = div(j, 1461) - 100100 + div(8 - gm, 6);
-  return {
-      gy: gy,
-      gm: gm,
-      gd: gd
-  }
+    var j
+      , i
+      , gd
+      , gm
+      , gy
+    j = 4 * jdn + 139361631
+    j = j + div(div(4 * jdn + 183187720, 146097) * 3, 4) * 4 - 3908
+    i = div(mod(j, 1461), 4) * 5 + 308
+    gd = div(mod(i, 153), 5) + 1
+    gm = mod(div(i, 153), 12) + 1
+    gy = div(j, 1461) - 100100 + div(8 - gm, 6)
+    return {
+        gy: gy
+            , gm: gm
+            , gd: gd
+    }
 }
 
 /*
@@ -235,9 +247,9 @@ function d2g(jdn) {
 */
 
 function div(a, b) {
-    return ~~(a / b);
+    return ~~(a / b)
 }
 
 function mod(a, b) {
-    return a - ~~(a / b) * b;
+    return a - ~~(a / b) * b
 }
